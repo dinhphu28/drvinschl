d2f457df093fadc20dd09fab6acd5f9513e869ef
Dinh Phu Nguyen <dinhphu28@dinhphu28.com>
2026-05-18 09:27:58 +0700

    Initialization
    Signed-off-by: Dinh Phu Nguyen <dinhphu28@dinhphu28.com>

---
+++ src/test/java/com/dinhphu28/drvinschl/DrvinschlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinhphu28.drvinschl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrvinschlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
