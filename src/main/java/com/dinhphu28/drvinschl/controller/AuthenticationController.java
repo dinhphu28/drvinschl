ca14d3c6a572053c5e002c56fc82e4dfd89dbba3
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-05-20 17:02:38 +0700

    Add cors setup
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- src/main/java/com/dinhphu28/drvinschl/controller/AuthenticationController.java
+++ src/main/java/com/dinhphu28/drvinschl/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
