f424a10f27aab7420b2ac8e3de2d55601cb1cf34
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-05-22 10:24:17 +0700

    Implement generate unique username
    - When login with Google
    
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- src/main/java/com/dinhphu28/drvinschl/repository/UserRepository.java
+++ src/main/java/com/dinhphu28/drvinschl/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.dinhphu28.drvinschl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.dinhphu28.drvinschl.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u.username FROM User u WHERE u.username LIKE CONCAT(:base, '%')")
+    List<String> findAllUsernamesStartingWith(@Param("base") String base);
 }
